ed23a90aadf02782c47440679cedee06bb6715e6
Tyranzx <70720366+Lewysan@users.noreply.github.com>
2022-04-09 17:56:04 +0200

    Update sattack.sh

--- sattack.sh
+++ sattack.sh
@@ -20,12 +20,17 @@ gray="\e[0;37m\033[1m"
 blue="\e[0;34m\033[1m"
 yellow="\e[0;33m\033[1m"
 
-# Bold Colors
+# Bold
+BBlack='\033[1;30m'
 BRed='\033[1;31m'
 BGreen='\033[1;32m'
 BYellow='\033[1;33m'
+BBlue='\033[1;34m'
+BPurple='\033[1;35m'
+BCyan='\033[1;36m'
+BWhite='\033[1;37m'
 
-# EX: ./sattack.sh -i 164.132.119.73 -p 25565 -t 5 -m true -s minecraft
+# EX: ./sattack.sh -i 164.132.119.73 -p 25565 -x '-v -A -T4'
 
 function helpUsage(){
 clear
@@ -38,8 +43,9 @@ echo -e " ▒██████▒▒ ▓█   ▓██▒ ▒██▒ ░   �
 echo -e " ▒ ▒▓▒ ▒ ░ ▒▒   ▓▒█░ ▒ ░░     ▒ ░░    ▒▒   ▓▒█░ ░▒ ▒  ░▒ ▒▒ ▓▒ "
 echo -e " ░ ░▒  ░ ░  ▒   ▒▒ ░   ░        ░      ▒   ▒▒ ░ ░  ▒   ░ ░▒ ▒░ "
 echo -e " ░  ░  ░    ░   ▒    ░        ░        ░   ▒  ░        ░ ░░ ░  "
-echo -e "       ░        ░  ░                       ░  ░ ░      ░  ░    "                                            ░                "
-echo -e "\n\n${BRed}✖ ${white}Uso correcto: ${0} [-i <address>] [-p <port>] [-x <other_parameters> (Optional)]"
+echo -e "       ░        ░  ░                       ░  ░ ░      ░  ░    "
+echo -e "                                              ░                "
+echo -e "\n${BRed}✖ ${white}Uso correcto: ${0} [-i <address>] [-p <port>] [-x <other_parameters> (Optional)]"
 echo -e "\t${green}[${yellow}+${green}]${white} i = Address | p = Port"
 echo -e "\n${BYellow}⚠️ ${white}Otro: \n\t${white}Ejemplo de puertos: \n\t\t${yellow}[${red}*${yellow}] ${white}445\n\t\t${yellow}[${red}*${yellow}] ${white}139"
 echo -e "\t\t${yellow}[${red}*${yellow}] ${white}3306"
@@ -53,7 +59,10 @@ exit 1
 
 trap ctrl_c INT
 
-function ctrl_c(){ echo -e "\n${red} Cerrando script.."; sleep 0.5; exit 0 }
+function ctrl_c(){
+echo " "
+echo -e "${red} Cerrando script.."; sleep 0.5; exit 0
+}
 
 parameter_counter=0; while getopts ":i:p:x:" o; do
 	case "${o}" in
@@ -76,17 +85,20 @@ function atacar(){
 	sleep 3; touch ~/.config/bin/sattack_target; nmap -p ${p} ${i} ${x} -oG ~/.config/bin/sattack_target
         sleep 1; clear
         echo -e "\n${BGreen}✔ ${white}Se han escaneado los puertos correctamente. Leyendo resultados de ${yellow}~/.config/bin/sattack_target${white}.."
-        sleep 2; echo -ne "\n${green}▶———————————————————————————————————————————◀${white}\n\n"
+        sleep 2
+        echo -ne "\n${green}▶———————————————————————————————————————————◀${white}\n\n"
         grep -v ^\# ~/.config/bin/sattack_target | tr '/' ' '; echo -ne "\n${green}▶———————————————————————————————————————————◀\n"; exit 0
 }
 function atacarSinObjetivo(){
-        clear; echo -e "\n${white} ◆ ${red}Objetivo: ${white}${i}"
+        clear
+        echo -e "\n${white} ◆ ${red}Objetivo: ${white}${i}"
         echo -e "${white} ◆ ${red}Puerto Especificado: ${white}${p}\n ${white}◆ ${red}Otro especificado: ${white}${x}"
         sleep 1; echo -e "\n\n${white}[${green}!${white}]${white} Escaneando puertos..\n${clear}"
         sleep 3; touch ~/.config/bin/sattack_target; nmap -p ${p} ${i} -oG ~/.config/bin/sattack_target
         sleep 1; clear
         echo -e "\n${BGreen}✔ ${white}Se han escaneado los puertos correctamente. Leyendo resultados de ${yellow}~/.config/bin/sattack_target${white}.."
-        sleep 2; echo -ne "\n${green}▶———————————————————————————————————————————◀${white}\n\n"
+        sleep 2
+        echo -ne "\n${green}▶———————————————————————————————————————————◀${white}\n\n"
         grep -v ^\# ~/.config/bin/sattack_target | tr '/' ' '; echo -ne "\n${green}▶———————————————————————————————————————————◀\n"; exit 0
 }
 
